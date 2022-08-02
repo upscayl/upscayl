0e1e2d1229a82add7d30e5ac8fe903964452b3a6
Nayam Amarshe <25067102+NayamAmarshe@users.noreply.github.com>
2022-08-02 14:28:24 +0530

    Init

---
+++ main/index.js
@@ -0,0 +1,40 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+// Native
+const path_1 = require("path");
+const url_1 = require("url");
+// Packages
+const electron_1 = require("electron");
+const electron_is_dev_1 = __importDefault(require("electron-is-dev"));
+const electron_next_1 = __importDefault(require("electron-next"));
+// Prepare the renderer once the app is ready
+electron_1.app.on('ready', async () => {
+    await (0, electron_next_1.default)('./renderer');
+    const mainWindow = new electron_1.BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: false,
+            contextIsolation: false,
+            preload: (0, path_1.join)(__dirname, 'preload.js'),
+        },
+    });
+    const url = electron_is_dev_1.default
+        ? 'http://localhost:8000/'
+        : (0, url_1.format)({
+            pathname: (0, path_1.join)(__dirname, '../renderer/out/index.html'),
+            protocol: 'file:',
+            slashes: true,
+        });
+    mainWindow.loadURL(url);
+});
+// Quit the app once all windows are closed
+electron_1.app.on('window-all-closed', electron_1.app.quit);
+// listen the channel `message` and resend the received message to the renderer process
+electron_1.ipcMain.on('message', (event, message) => {
+    console.log(message);
+    setTimeout(() => event.sender.send('message', 'hi from electron'), 500);
+});
---
+++ renderer/.next/static/webpack/webpack.2f5a673c60abf6d9.hot-update.js
@@ -0,0 +1,66 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdate_N_E"]("webpack",{},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ /* webpack/runtime/compat get default export */
+/******/ !function() {
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function() { return module['default']; } :
+/******/ 			function() { return module; };
+/******/ 		__webpack_require__.d(getter, { a: getter });
+/******/ 		return getter;
+/******/ 	};
+/******/ }();
+/******/ 
+/******/ /* webpack/runtime/define property getters */
+/******/ !function() {
+/******/ 	// define getter functions for harmony exports
+/******/ 	__webpack_require__.d = function(exports, definition) {
+/******/ 		for(var key in definition) {
+/******/ 			if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 				Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 			}
+/******/ 		}
+/******/ 	};
+/******/ }();
+/******/ 
+/******/ /* webpack/runtime/getFullHash */
+/******/ !function() {
+/******/ 	__webpack_require__.h = function() { return "f320c99a8c730992"; }
+/******/ }();
+/******/ 
+/******/ /* webpack/runtime/make namespace object */
+/******/ !function() {
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/ }();
+/******/ 
+/******/ /* webpack/runtime/compat */
+/******/ 
+/******/ 
+/******/ // noop fns to prevent runtime errors during initialization
+/******/ if (typeof self !== "undefined") {
+/******/ 	self.$RefreshReg$ = function () {};
+/******/ 	self.$RefreshSig$ = function () {
+/******/ 		return function (type) {
+/******/ 			return type;
+/******/ 		};
+/******/ 	};
+/******/ }
+/******/ 
+/******/ }
+);
