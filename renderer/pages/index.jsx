20877e62aa12698647210742a5b7020161e80fb2
TGS963 <ghoshsuvojit2012@gmail.com>
2022-08-19 14:27:05 +0530

    added anime button

--- renderer/pages/index.jsx
+++ renderer/pages/index.jsx
@@ -39,6 +39,12 @@ const Home = () => {
     }
   };
 
+  const handleCheck = (e) => {
+    if (e.target.checked) {
+      setModel("realesrgan-x4plus-anime")
+    }
+  }
+
   const outputHandler = async () => {
     var path = await window.electron.invoke(commands.SELECT_FOLDER);
     if (path !== "cancelled") {
@@ -83,6 +89,8 @@ const Home = () => {
               Select Image
             </button>
           </div>
+          <input className="appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="checkbox" onChange={handleCheck}/>
+          <label className="text-neutral-100" >Anime</label>
 
           {/* STEP 2 */}
           <div className="mt-10">
