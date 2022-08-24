ab1b8faab12756291b600dcb6f128afc545ce332
Feenix <25067102+NayamAmarshe@users.noreply.github.com>
2022-08-24 07:38:57 +0530

    Deleted commands

--- constants/commands.js
+++
@@ -1,9 +0,0 @@
-const commands = {
-  SELECT_FILE: "Select a File",
-  SELECT_FOLDER: "Select a Folder",
-  UPSCAYL: "Upscale the Image",
-  UPSCAYL_DONE: "Upscaling Done",
-  UPSCAYL_PROGRESS: "Send Progress from Main to Renderer",
-};
-
-module.exports = commands;
