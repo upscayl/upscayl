56af5c780ab633a26f94c165b9274a57e9de301b
Nayam Amarshe <25067102+NayamAmarshe@users.noreply.github.com>
2022-08-27 15:55:23 +0530

    Fixed progress bug

--- renderer/pages/index.jsx
+++ renderer/pages/index.jsx
@@ -19,6 +19,7 @@ const Home = () => {
   const [version, setVersion] = useState("");
 
   const resetImagePaths = () => {
+    setProgress("");
     SetImagePath("");
     setUpscaledImagePath("");
   };
