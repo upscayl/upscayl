7c08bb5e62d1381c8a180986824420d76db4d463
TGS963 <ghoshsuvojit2012@gmail.com>
2022-08-29 19:12:52 +0530

    rolled back commands.js

--- main/commands.js
+++ main/commands.js
@@ -1,7 +1,6 @@
 const commands = {
   SELECT_FILE: "Select a File",
   SELECT_FOLDER: "Select a Folder",
-  FILE_EXISTING: "File exists",
   UPSCAYL: "Upscale the Image",
   UPSCAYL_DONE: "Upscaling Done",
   UPSCAYL_PROGRESS: "Send Progress from Main to Renderer",
