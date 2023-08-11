a4a368b1279ba815596cc80401e41401602f8d01
Nayam Amarshe <25067102+NayamAmarshe@users.noreply.github.com>
2023-08-11 15:17:28 +0530

    Update button

--- renderer/pages/index.tsx
+++ renderer/pages/index.tsx
@@ -670,7 +670,7 @@ const Home = () => {
               All done!
             </p>
             <button
-              className="btn bg-gradient-blue rounded-lg p-3 font-medium text-white/90 transition-colors"
+              className="btn btn-primary bg-gradient-blue rounded-lg p-3 font-medium text-white/90 transition-colors"
               onClick={openFolderHandler}>
               Open Upscayled Folder
             </button>
