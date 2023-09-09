319501aab147e157b67a1826c562c71ab768615b
Nayam Amarshe <25067102+NayamAmarshe@users.noreply.github.com>
2023-09-09 19:31:58 +0530

    Fix #380

--- renderer/components/settings-tab/GpuIdInput.tsx
+++ renderer/components/settings-tab/GpuIdInput.tsx
@@ -9,6 +9,12 @@ export function GpuIdInput({ gpuId, handleGpuIdChange }) {
   return (
     <div className="flex flex-col gap-2">
       <p className="text-sm font-medium">GPU ID</p>
+      {window.electron.platform === "win" && (
+        <p className="text-xs text-base-content/80">
+          Please enable performance mode on Windows for better results. Please
+          read the Upscayl Wiki for more information.
+        </p>
+      )}
       <input
         type="text"
         placeholder="Type here"
