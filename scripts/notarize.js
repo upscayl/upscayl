5869d72cc9bf7b86b2e72f3df2e927e3ed018980
Nayam Amarshe <25067102+NayamAmarshe@users.noreply.github.com>
2023-09-21 13:57:58 +0530

    Update notarize script

--- scripts/notarize.js
+++ scripts/notarize.js
@@ -10,7 +10,7 @@ exports.default = async function notarizing(context) {
   const appName = context.packager.appInfo.productFilename;
 
   return await notarize({
-    appBundleId: "io.datasette.app",
+    appBundleId: "org.upscayl.Upscayl",
     appPath: `${appOutDir}/${appName}.app`,
     appleId: process.env.APPLEID,
     appleIdPassword: process.env.APPLEIDPASS,
