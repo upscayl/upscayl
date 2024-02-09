8ef5348261ec1882640c0ebdd43b5482c25e00ae
Nayam Amarshe <25067102+NayamAmarshe@users.noreply.github.com>
2024-02-09 17:29:41 +0530

    Update binary script

--- update_upscayl_ncnn_binaries.sh
+++ update_upscayl_ncnn_binaries.sh
@@ -1,19 +1,43 @@
 #!/bin/bash
 
+# Step 1: Fetch JSON from GitHub API and get assets_url
+assets_url=$(curl -s https://api.github.com/repos/upscayl/upscayl-ncnn/releases/latest | jq -r '.assets_url')
 
-# Step 1: Download all the zip files from the GitHub releases page
-curl -s https://api.github.com/repos/upscayl/upscayl-ncnn/releases/latest | grep "realesrgan-ncnn-vulkan" | cut -d : -f 2,3 | tr -d \" | wget -qi - > /dev/null 2>&1
+# Step 2: Loop through each asset and download the files
+curl -s $assets_url | jq -r '.[] | .browser_download_url' | while read -r download_url; do
+    filename=$(basename $download_url)
+    echo "Downloading $filename..."
+    curl -LO $download_url
+done
 
-# Get the latest version number
-VERSION=curl -s https://api.github.com/repos/upscayl/upscayl-ncnn/releases/latest | jq -r ".tag_name"
-echo "Latest version: $VERSION"
+# Step 3: Extract downloaded ZIP files to a specific folder
+mkdir -p extracted_files
+for file in *.zip; do
+    echo "Extracting $file..."
+    unzip -q $file -d extracted_files
+done
 
-# Step 2: Extract the required zip files to their respective directories
-unzip -j ./realesrgan-ncnn-vulkan-$VERSION-ubuntu.zip -d resources/linux/bin
-# unzip realesrgan-ncnn-vulkan-$VERSION-macos.zip -d resources/macos/bin
-# unzip realesrgan-ncnn-vulkan-$VERSION-windows.zip -d resources/windows/bin
+# Step 4: Move files to respective folders
+for folder in extracted_files/upscayl-bin-*; do
+	if [[ -d $folder ]]; then
+		platform=$(echo "extracted_files/$folder" | cut -d '-' -f 5)
+		echo "Moving files in $folder to $platform folder..."
+		if [[ "$platform" == "linux" ]]; then
+			cp "$folder"/upscayl-bin resources/linux/bin/upscayl-bin
+		elif [[ "$platform" == "macos" ]]; then
+			cp "$folder"/upscayl-bin resources/mac/bin/upscayl-bin
+		elif [[ "$platform" == "windows" ]]; then
+			cp "$folder"/upscayl-bin.exe resources/win/bin/upscayl-bin.exe
+			cp "$folder"/vcomp140.dll resources/win/bin/vcomp140.dll
+			cp "$folder"/vcomp140d.dll resources/win/bin/vcomp140d.dll
+		fi
+	fi
+done
 
-# Step 4: Clean up the zip files
-# rm *.zip
+echo "All files moved to their respective folders successfully."
+
+# Step 5: Clean up extracted_files folder and downloaded ZIP files
+rm -rf extracted_files
+rm -f *.zip
 
 echo "Script executed successfully."
