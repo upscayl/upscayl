0dd5b029d07d54a84d75790313dce0e4b0843736
Nayam Amarshe <25067102+NayamAmarshe@users.noreply.github.com>
2024-04-25 09:18:48 +0530

    Fix windows separator

--- common/get-file-name.ts
+++ common/get-file-name.ts
@@ -1,8 +1,5 @@
-export default function getFilenameFromPath(
-  path: string,
-  withExtension: boolean = true,
-) {
+export default function getFilenameFromPath(path: string) {
   if (!path) return "";
-  if (withExtension) return path.split("/").slice(-1)[0];
-  return path.split("/").slice(-1)[0].split(".").slice(0, -1).join(".");
+  const separator = path.includes("/") ? "/" : "\\";
+  return path.split(separator).slice(-1)[0];
 }
