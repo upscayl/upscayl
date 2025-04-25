eb9f1348697c57d651a63839f1a857c5c08f1984
Pedro Medina <112674317+medinapdr@users.noreply.github.com>
2025-04-25 10:23:15 -0300

    fix: enforce Windows filename length limit check only on Win32 platform (#1193)

--- renderer/pages/index.tsx
+++ renderer/pages/index.tsx
@@ -175,6 +175,13 @@ const Home = () => {
         setProgress(t("APP.PROGRESS.PROCESSING_TITLE"));
       },
     );
+    // UPSCAYL WARNING
+    window.electron.on(ELECTRON_COMMANDS.UPSCAYL_WARNING, (_, data: string) => {
+      toast({
+        title: t("WARNING.GENERIC_WARNING.TITLE"),
+        description: data,
+      });
+    });
     // UPSCAYL ERROR
     window.electron.on(ELECTRON_COMMANDS.UPSCAYL_ERROR, (_, data: string) => {
       toast({
